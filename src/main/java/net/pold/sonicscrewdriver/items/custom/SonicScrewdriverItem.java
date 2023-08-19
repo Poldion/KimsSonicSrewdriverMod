9f87cbd8bc625c09314f5780367a9c60e662beac
Josh <joshstruck1997@gmail.com>
2023-08-20 00:59:00 +0200

    0.1.2

--- src/main/java/net/pold/sonicscrewdriver/items/custom/SonicScrewdriverItem.java
+++ src/main/java/net/pold/sonicscrewdriver/items/custom/SonicScrewdriverItem.java
@@ -1,12 +1,18 @@
 package net.pold.sonicscrewdriver.items.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RedstoneTorchBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.event.level.BlockEvent;
 
 public class SonicScrewdriverItem extends Item {
     public SonicScrewdriverItem(Properties properties) {
@@ -21,6 +27,22 @@ public class SonicScrewdriverItem extends Item {
             Player player = pContext.getPlayer();
             BlockState stat = pContext.getLevel().getBlockState(positionClicked);
 
+            // Get the direction of the clicked block face
+            Direction direction = pContext.getClickedFace();
+
+            // Find the next air block along that direction
+            BlockPos nextAirBlock = positionClicked;
+            while (pContext.getLevel().getBlockState(nextAirBlock).getBlock() != Blocks.AIR) {
+                nextAirBlock = nextAirBlock.relative(direction);
+            }
+
+            // Replace the air block with a redstone block only if it is an air block
+            BlockState nextBlockState = pContext.getLevel().getBlockState(nextAirBlock);
+            if (nextBlockState.getBlock() == Blocks.AIR) {
+                BlockState redstoneBlock = Blocks.REDSTONE_WALL_TORCH.defaultBlockState();
+                pContext.getLevel().setBlockAndUpdate(nextAirBlock, redstoneBlock);
+            }
+
             outputCoordinates(positionClicked, player);
         }
 
@@ -28,6 +50,9 @@ public class SonicScrewdriverItem extends Item {
 
         return InteractionResult.SUCCESS;
     }
+
+
+
 
     private void outputCoordinates(BlockPos blockPos, Player player) {
         player.sendSystemMessage(Component.literal(blockPos.getX() + "," + blockPos.getY() + "," + blockPos.getZ()));
