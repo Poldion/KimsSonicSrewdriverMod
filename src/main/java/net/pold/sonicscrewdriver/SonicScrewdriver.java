e2eb9c99eca654e5fd17ab9a41847267661d07c7
Josh <joshstruck1997@gmail.com>
2023-08-19 16:43:19 +0200

    0.1.0

--- src/main/java/net/pold/sonicscrewdriver/SonicScrewdriver.java
+++ src/main/java/net/pold/sonicscrewdriver/SonicScrewdriver.java
@@ -1,6 +1,7 @@
 package net.pold.sonicscrewdriver;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +14,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.pold.sonicscrewdriver.items.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -26,6 +28,8 @@ public class SonicScrewdriver {
     public SonicScrewdriver() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -37,8 +41,7 @@ public class SonicScrewdriver {
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
@@ -47,7 +50,9 @@ public class SonicScrewdriver {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.SONICSCREWDRIVER);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/pold/sonicscrewdriver/items/ModItems.java
+++ src/main/java/net/pold/sonicscrewdriver/items/ModItems.java
@@ -1,5 +1,21 @@
 package net.pold.sonicscrewdriver.items;
 
-public class ModItems {
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.pold.sonicscrewdriver.SonicScrewdriver;
 
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SonicScrewdriver.MODID);
+
+
+    public static final RegistryObject<Item> SONICSCREWDRIVER = ITEMS.register("sonicscrewdriver",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
